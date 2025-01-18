a0c0472b60da950e5a25a00dcddd81cb084fe1d2
asaminiko <chebakova29@gmail.com>
2025-01-19 00:05:47 +0200

    added comments

--- task2_currency/script.js
+++ task2_currency/script.js
@@ -1,25 +1,27 @@
 function calculation() {
   try {
-    let oneUah = parseFloat(prompt('Введи, скільки гривень коштує долар'))
-    let checkedOneUah = check(oneUah)
-    for (let i = 1; i < 11; i++) {
+    let oneUah = parseFloat(prompt('Введи, скільки гривень коштує долар')) //запитує число
+    let checkedOneUah = check(oneUah) //перевіряє
+    console.log(`1 долар це ${checkedOneUah} грн`)
+    for (let i = 1; i <= 10; i++) {
       let usd = i * 10
       let uah = usd * checkedOneUah
       console.log(`${usd} доларів це ${uah} грн`)
     }
   } catch (error) {
-    console.error(`Помилка: ${error.message}`)
+    console.error(`Помилка: ${error.message}`) //може бути помилка
   } finally {
-    console.log('Перевірено')
+    console.log('Перевірено') //тобто завжди перевірить
   }
 }
 
 function check(y) {
-  if (y === undefined || y === null) throw new TypeError('Невірний тип даних.')
+  if (y === undefined || y === null) throw new TypeError('Невірний тип даних.') //перевірка на undefined, null
   if (typeof y === 'string' && y.trim() === '')
+    //перевірка на пусту строку
     throw new TypeError('Пуста строка.')
-  if (isNaN(y)) throw new ReferenceError('Значення не є числом.')
-  if (y > 50 || y < 1) throw new RangeError('Число більше 50 або менше 1')
+  if (isNaN(y)) throw new ReferenceError('Значення не є числом.') //перевірка на isNaN()
+  if (y > 50 || y < 1) throw new RangeError('Число більше 50 або менше 1') //перевірка чи число більше 50 або менше 1
 
   return y
 }
--- task3_searchN/script.js
+++ task3_searchN/script.js
@@ -1,24 +1,25 @@
 function search() {
   try {
-    let number = parseInt(prompt('Введи ціле число'))
+    let number = parseInt(prompt('Введи ціле число')) //запитує ціле число
     let checkedNumber = check(number)
     console.log(`Введене число ${checkedNumber}`)
     for (let i = 1; Math.pow(i, 2) <= checkedNumber; i++) {
       console.log(`${i}`)
     }
   } catch (error) {
-    console.error(`Помилка: ${error.message}`)
+    console.error(`Помилка: ${error.message}`) //може бути помилка
   } finally {
-    console.log('Перевірено')
+    console.log('Перевірено') //тобто завжди перевірить
   }
 }
 
 function check(y) {
-  if (y === undefined || y === null) throw new TypeError('Невірний тип даних.')
+  if (y === undefined || y === null) throw new TypeError('Невірний тип даних.') //перевірка на undefined, null
   if (typeof y === 'string' && y.trim() === '')
+    //перевірка на пусту строку
     throw new TypeError('Пуста строка.')
-  if (isNaN(y)) throw new ReferenceError('Значення не є числом.')
-  if (y > 10000 || y < 1) throw new RangeError('Число більше 10000 або менше 1')
+  if (isNaN(y)) throw new ReferenceError('Значення не є числом.') //перевірка на isNaN()
+  if (y > 10000 || y < 1) throw new RangeError('Число більше 10000 або менше 1') //перевірка чи число більше 10000 або менше 1
 
   return y
 }
