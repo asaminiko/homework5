a263b0eea08959380dae6ea75f07ae975fc84c6b
asaminiko <chebakova29@gmail.com>
2025-01-24 00:20:02 +0200

    алгоритм без обмеження

--- task4_primeNum/script1.js
+++ task4_primeNum/script1.js
@@ -1,33 +1,39 @@
-let number = parseInt(prompt('Введи число від 2 до 50'))
-let checkedNumber = check(number)
-if (checkedNumber != 0) {
-  primeNumber(checkedNumber)
-} else {
-  console.log('кінець')
+try {
+  let number = check(prompt('Введіть ціле число'))
+  let isPrime = primeNumber(number)
+  if (isPrime) {
+    console.log(`Число ${number} просте`)
+  } else {
+    console.log(`Число ${number} не просте`)
+  }
+} catch (error) {
+  console.error(`Помилка ${error}`)
+} finally {
+  console.log('Перевірено')
 }
-
 //чи є число простим?
 function primeNumber(num) {
-  for (let i = 2; i <= 10; i++) {
-    if (num != i && num % i === 0) {
+  if (num < 2) return false //число менше 2 - не просте
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
       console.log(` ${num} % ${i} = ${num % i}`)
-      return console.log(`${num} не просте`)
+      return false
     }
   }
-  console.log(`${num} просте`)
+  return true
 }
 
 //перевірка
 function check(y) {
-  if (y === undefined || y === null || isNaN(y) || y > 50 || y < 2) {
-    //перевірка на undefined, null, isNaN(), число більше 50 або менше 1
-    console.log('помилка')
-    return 0
-  } else if (typeof y === 'string' && y.trim() === '') {
-    //перевірка на пусту строку
-    console.log('помилка')
-    return 0
-  } else {
-    return y
-  }
+  if (
+    y === undefined ||
+    y === null ||
+    (typeof y === 'string' && y.trim() === '')
+  )
+    throw new TypeError('Невірний тип даних.') //перевірка на undefined, null, пусту строку
+  if (typeof y === 'string' && isNaN(Number(y)))
+    throw new TypeError('Значення не є числом.') //перевірка на isNaN()
+  if (!Number.isInteger(Number(y))) throw new TypeError('Не ціле число.') //перевірка на цілі числа
+
+  return Number(y)
 }
